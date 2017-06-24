e1ed04d0ba383dddb660da8eda09925a569dfb05
Cixtor <cixtords@gmail.com>
2017-06-24 09:00:00 -0500

    Add method to handle the reaction for help requests

--- handlers.go
+++ handlers.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
+	"os"
+	"regexp"
 	"strconv"
 	"strings"
 
@@ -29,5 +32,42 @@ func (s *Slackbot) HandleMessage(event *slackapi.MessageEvent) {
 		return
 	}
 
+	if s.HandleHelp(event) {
+		s.LogCommand(event)
+		return
+	}
+
 	s.LogMessage(event)
+}
+
+// HandleHelp reacts to an uptime request.
+func (s *Slackbot) HandleHelp(event *slackapi.MessageEvent) bool {
+	if event.Text == "help" {
+		file, err := os.Open(s.ReadmeFile)
+		if err != nil {
+			log.Println("readme open;\x20", err)
+			return false
+		}
+
+		body, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.Println("readme read;\x20", err)
+			return false
+		}
+
+		reply := string(body)
+		reHeading := regexp.MustCompile(`### (.+)`)
+		reMention := regexp.MustCompile(`\*\*([@#]\S+)\*\*`)
+		reAnchors := regexp.MustCompile(`\[([^\]]+)\]\(([^\)]+)\)`)
+
+		reply = reHeading.ReplaceAllString(reply, `*$1*`)
+		reply = reMention.ReplaceAllString(reply, `$1`)
+		reply = reAnchors.ReplaceAllString(reply, `<$2|$1>`)
+
+		session := s.Client.InstantMessageOpen(event.User)
+		s.Client.ChatPostMessage(session.Channel.ID, reply)
+		return true
+	}
+
+	return false
 }
